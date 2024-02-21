3e65519b9c3ad7ba4077fedd0a1d771321a0fd20
vivi <vaishu.josyula@gmail.com>
2024-02-20 19:05:16 -0600

    menu for cube and sphere

--- Assets/changeSphereColor.cs
+++ Assets/changeSphereColor.cs
@@ -8,24 +8,36 @@ public class changeSphereColor : MonoBehaviour
     public GameObject sphere;
     private bool changeColorMode = false;
     private bool scalingMode = false;
+    private Transform sphereTransform;
 
 
     // Start is called before the first frame update
     void Start()
     {
         sphereRenderer = GetComponent<Renderer>();
+        sphereTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // need to change
+
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("B PRESSED");
+            ScaleCube();
+        }
         if (Input.GetKeyDown(KeyCode.A))
         {
             ChangeColor();
-
         }
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ClearAction();
+        }
+
     }
 
     public void ChangeColor()
@@ -56,13 +68,20 @@ public class changeSphereColor : MonoBehaviour
 
     public void ScaleCube()
     {
-        sphere.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f); 
+        sphere.transform.localScale += new Vector3(.1f, .1f, .1f);
+        Debug.Log("SCALING");
     }
 
+    // change to only clear previously selected mode
     public void ClearAction()
     {
+        Color blueColor = HexToColor("#62E5FF");
         changeColorMode = false;
         scalingMode = false;
+        sphereRenderer.material.color = blueColor;
+        sphereTransform = GetComponent<Transform>();
+        sphereTransform.localScale = new Vector3(1f, 1f, 1f);
+
     }
 
     public void SetChangeColorMode()
